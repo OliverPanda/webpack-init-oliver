22310c661a4597f4e66c2c4f34ffd265d73789fb
OliverPanda <380425169@qq.com>
2018-05-23 10:03:50 +0800

    Add files via upload

--- template/src/getData/getData.js
+++ template/src/getData/getData.js
@@ -5,6 +5,6 @@ fetch.baseUrl = '这是本项目的API地址'
 // 可以使用fetch来获取数据, 如下面例子，引入cityGuess的时候只需要import {cityGuess} from getData的路径
 /*
 export const cityGuess = () => fetch('/v1/cities', {
-	type: 'guess'
+  type: 'guess'
 });
 */
